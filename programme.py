a9c2348f2f832622da2af8811417b2a9cc2501a0
farahc123 <fcheded@gmail.com>
2025-01-06 16:08:45 +0000

    Update programme.py
    Added user sarah to test

--- programme.py
+++ programme.py
@@ -43,6 +43,7 @@ events = [
     Event('2020-01-22 10:25:34', 'logout', 'myworkstation.local', 'jordan'),
     Event('2020-01-21 08:20:01', 'login', 'webserver.local', 'jordan'),
     Event('2020-01-23 11:24:35', 'logout', 'mailserver.local', 'chris'),
+    Event('2020-01-24 11:24:35', 'login', 'admin.local', 'sarah'),
 ]
 
 users = current_users(events)
